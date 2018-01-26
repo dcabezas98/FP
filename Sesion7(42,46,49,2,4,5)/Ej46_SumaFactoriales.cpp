945ce3cca7306a382448a65a28318c0766610523
David Cabezas <dxabezas@gmail.com>
2018-01-26 19:06:52 +0100

    Sesion 7

---
+++ Sesion7(42,46,49,2,4,5)/Ej46_SumaFactoriales.cpp
@@ -0,0 +1,32 @@
+/* Programa que calcula la sumatoria de los factoriales
+	desde uno a un valor T introducido. */
+	
+	#include<iostream>
+	
+	using namespace std;
+	
+	int main () {
+		
+		int T;
+		long long factorial, suma = 0;
+		
+		do {
+		cout << "¿Hasta qué numero quiere calcular la sumatoria de factoriales? ";
+		cin >> T;
+		} while ( T < 0);
+		
+		for (int contador1 = 1; contador1 <= T; contador1++) {
+			
+			factorial = 1;
+			
+			for (int contador2 = 2; contador2 <= contador1; contador2++) {                          // La interación con contador2 = 1 se puede omitir.
+				
+				factorial *= contador2;
+			}
+			
+			suma += factorial;
+		}
+		
+		cout << "La suma de factoriales desde 1 hasta " << T << " es: " << suma << endl;
+	}
+		
---
+++ Sesion7(42,46,49,2,4,5)/Ejer4_LeerValoresIntervalo(func).cpp
@@ -0,0 +1,43 @@
+/* Programa que usa una función para leer dentro de un intervalo
+	3 valores y los suma. */
+
+#include<iostream>
+
+using namespace std;
+
+int LeeIntRango (int min, int max) {
+
+	int numero;
+	
+	do {
+		cout << "Inserte número entre " << min << " y " << max << ": ";
+		cin >> numero;
+	} while ( numero < min || numero > max);
+	
+	return numero;
+}
+	
+int main () {
+	
+	int min, max, sumando, suma;
+	const int NO_SUMANDOS = 3;
+	
+	do {
+		cout << "Inserte valor mínimo del intervalo: ";
+		cin >> min;
+	} while (min < 0);
+	
+		cout << "Inserte valor máximo del intervalo: ";
+		cin >> max;
+	
+	suma = 0;
+	
+	for (int contador = 1; contador <= NO_SUMANDOS; contador++) {
+		
+		sumando = LeeIntRango(min, max);
+		suma += sumando;
+	}
+	
+	cout << "La suma de los tres valores introducidos es : " << suma << endl;
+}
+	
