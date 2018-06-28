7e863958be82365e2f6ce73bc2e395a81b7d6ad6
dcabezas98 <dxabezas@gmail.com>
2018-06-28 14:48:07 +0200

    Sesion9: EOL a LF (Unix) y tildes

--- Sesion9/Fibonacci.cpp
+++ Sesion9/Fibonacci.cpp
@@ -1,7 +1,7 @@
-/* Programa que calcula un término n de la sucesión de Fibonacci
-   y los k términos siguientes de la sucesión usando una función 
+/* Programa que calcula un tÃ©rmino n de la sucesiÃ³n de Fibonacci
+   y los k tÃ©rminos siguientes de la sucesiÃ³n usando una funciÃ³n
    recursiva.	*/
-	
+
 #include<iostream>
 
 using namespace std;
@@ -9,34 +9,34 @@ using namespace std;
 int LeePositivo() {
   int numero;
   do {
-    cout << "Inserte número mayor que 0: ";
+    cout << "Inserte nÃºmero mayor que 0: ";
     cin >> numero;
   } while (numero <= 0);
   return numero;
 }
-	
+
 long long Fibonacci(int n) {
 
-  if (n <=2) 
+  if (n <=2)
     return 1;
   else
     return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
- 	
+
 int main() {
   int n, k;
   long long fn;
-	
-  cout << "¿Qué término de la sucesión de Fibonacci quiere calcular? " << endl;
+
+  cout << "Â¿QuÃ© tÃ©rmino de la sucesiÃ³n de Fibonacci quiere calcular? " << endl;
   n = LeePositivo();
-	
+
   fn = Fibonacci(n);
-	
-  cout << "El número de Fibonacci de orden " << n << " es " << fn << endl;
-	
-  cout << "¿Cuántos términos de la sucesión de Fibonacci a partir de " << n << " quiere calcular? " << endl;
-  k = LeePositivo(); 
-	
+
+  cout << "El nÃºmero de Fibonacci de orden " << n << " es " << fn << endl;
+
+  cout << "Â¿CuÃ¡ntos tÃ©rminos de la sucesiÃ³n de Fibonacci a partir de " << n << " quiere calcular? " << endl;
+  k = LeePositivo();
+
   for (int contador = 1; contador <= k; contador++) {
     fn = Fibonacci(n + contador);
     cout << "Fibonacci de orden " << n + contador << ": \t" << fn << endl;
--- Sesion9/Intercambia.cpp
+++ Sesion9/Intercambia.cpp
@@ -1,14 +1,14 @@
 /*
-  Función que utiliza el paso por referencia para intercambiar los
+  FunciÃ³n que utiliza el paso por referencia para intercambiar los
   valores de dos variables.  */
-	
+
 #include<iostream>
-	
+
 using namespace std;
-	
+
 void Intercambia (int &a, int &b) {
   int auxiliar;
-	
+
   auxiliar = a;
   a = b;
   b = auxiliar;
@@ -16,14 +16,14 @@ void Intercambia (int &a, int &b) {
 
 int main () {
   int a, b;
-	
+
   cout << "a: ";
   cin >> a;
   cout << "b: ";
   cin >> b;
-	
+
   Intercambia (a, b);
-	
+
   cout << "a: " << a << endl;
   cout << "b: " << b << endl;
 }
